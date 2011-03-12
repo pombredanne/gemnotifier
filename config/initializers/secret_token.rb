fa2d5ff701adc2030f2bf8ca80441fa20453a00e
rociiu <rociiu.yu@gmail.com>
2011-03-12 19:51:44 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemnotifier::Application.config.secret_token = '552c3671ef9c87a7bc3c78f317bb3e8f3a7c78bb03a3d97b40ca424692e492fb68e6e855c0ef635eefc66be92a2cccd6e7ca5bc2e68964e83c69c3b988a94940'
