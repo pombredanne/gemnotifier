addd9374e2471b9475fe8407f5a1434fb6f203a5
rociiu <rociiu.yu@gmail.com>
2011-03-13 18:23:07 +0800

    add basic gem search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,8 @@ class HomeController < ApplicationController
     @gem_items = GemItem.order(:name).page params[:page]
   end
 
+  def search
+    @gem_items = GemItem.where("name like '%#{params[:query]}%'").page params[:page]
+  end
+  
 end
