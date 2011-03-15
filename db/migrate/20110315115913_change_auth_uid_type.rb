8d6a8c0a01322417ddfece32d0b072d0534f5331
rociiu <rociiu.yu@gmail.com>
2011-03-15 20:02:55 +0800

    fix migration

--- db/migrate/20110315115913_change_auth_uid_type.rb
+++ db/migrate/20110315115913_change_auth_uid_type.rb
@@ -1,9 +1,23 @@
 class ChangeAuthUidType < ActiveRecord::Migration
   def self.up
-    change_column :authorizations, :uid, :integer
+    drop_table :authorizations
+    create_table :authorizations do |t|
+      t.string :provider
+      t.integer :uid
+      t.integer :user_id
+
+      t.timestamps
+    end
   end
 
   def self.down
-    change_column :authorizations, :uid, :string
+    drop_table :authorizations
+    create_table :authorizations do |t|
+      t.string :provider
+      t.string :uid
+      t.integer :user_id
+
+      t.timestamps
+    end
   end
 end
