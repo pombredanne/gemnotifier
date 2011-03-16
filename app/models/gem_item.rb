0ff2635cdc56d2512890279399572569575a1fa3
rociiu <rociiu.yu@gmail.com>
2011-03-16 20:03:01 +0800

    add subscribe relationship

--- app/models/gem_item.rb
+++ app/models/gem_item.rb
@@ -2,4 +2,8 @@ class GemItem < ActiveRecord::Base
   
   serialize :dependencies
   
+  def subscribers
+    User.joins("join subscribes on subscribes.user_id = users.id").where("subscribes.gem_item_id = ?", self.id)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_many :authorizations
+  
+  def subscribed_gems
+    GemItem.joins("join subscribes on subscribes.gem_item_id = gem_items.id").where("subscribes.user_id = ?", self.id)
+  end
+  
 end
