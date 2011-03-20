0e1a0dd25dbfd3f45c65a8757667a53795cf44d3
rociiu <rociiu.yu@gmail.com>
2011-03-20 19:55:37 +0800

    update style, add gem detail page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@ $(function(){
   $("a.sub_btn").live('click', function(){
     if(!logged_in) {
       alert("You must login to subscribe");
-      return
+      return;
     }
     var gem_id = $(this).attr("rel");
     var gem_ele = $(this).parent();
@@ -20,6 +20,10 @@ $(function(){
   });
   
   $("a.unsub_btn").live('click', function(){
+    if(!logged_in) {
+      alert("You must login to subscribe");
+      return;
+    }
     var gem_id = $(this).attr("rel");
     var gem_ele = $(this).parent();
     $(this).hide();
@@ -32,6 +36,48 @@ $(function(){
          gem_ele.replaceWith(response);
        }
      });
+  });
+  
+  $("#subscribe_btn").live('click', function(){
+    if(!logged_in) {
+      alert("You must login to subscribe");
+      return;
+    }
+    var gem_id = $(this).attr('rel');
+    $(this).css('background-color', '#999');
+    $(this).val('Subscribing...');
+    var ele = $(this);
+    $.ajax({
+      type: 'POST',
+      url: '/subscribe',
+      data: { gem_id: gem_id },
+      success:function() {
+        ele.attr('id', 'unsubscribe_btn');
+        ele.css('background-color', '#1a6184');
+        ele.val('Unsubscribe');
+      }
+    });
+  });
+  
+  $("#unsubscribe_btn").live('click', function(){
+    if(!logged_in) {
+      alert("You must login to subscribe");
+      return;
+    }
+    var gem_id = $(this).attr('rel');
+    $(this).css('background-color', '#999');
+    $(this).val('Unsubscribing...');
+    var ele = $(this);
+    $.ajax({
+      type: 'POST',
+      url: '/unsubscribe',
+      data: { gem_id: gem_id },
+      success:function() {
+        ele.attr('id', 'subscribe_btn');
+        ele.css('background-color', '#1a6184');
+        ele.val('Subscribe');
+      }
+    });
   });
   
 });
