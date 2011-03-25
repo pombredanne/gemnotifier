9c183718449311d4ea41edca7a8a51033acf0a1a
rociiu <rociiu.yu@gmail.com>
2011-03-25 21:40:19 +0800

    minor fix

--- app/controllers/gem_items_controller.rb
+++ app/controllers/gem_items_controller.rb
@@ -1,6 +1,9 @@
 class GemItemsController < ApplicationController
   def show
-    @gem_item = GemItem.find_by_name(params[:id])
+    @gem_item = GemItem.where(:name => params[:id]).first
+    if @gem_item.nil?
+      render :file => "#{Rails.root}/public/404.html", :status => :not_found 
+    end
   end
 
 end
