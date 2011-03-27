36d84bca45979c3a6cfd5e972f80a5699297847e
rociiu <rociiu.yu@gmail.com>
2011-03-27 20:36:54 +0800

    order by latest gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :authorizations
   
   def subscribed_gems
-    GemItem.joins("join subscribes on subscribes.gem_item_id = gem_items.id").where("subscribes.user_id = ?", self.id)
+    GemItem.joins("join subscribes on subscribes.gem_item_id = gem_items.id").where("subscribes.user_id = ?", self.id).order("subscribes.created_at desc")
   end
   
   def subscribed?(gem_item)
