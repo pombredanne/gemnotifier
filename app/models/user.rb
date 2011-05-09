ee6bfd7d343f6af12796f7e0798a0e193ed35fc9
rociiu <rociiu.yu@gmail.com>
2011-05-09 14:59:15 +0800

    add feature: upload with Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@ class User < ActiveRecord::Base
     Notification.joins("join gem_items on gem_items.id = notifications.gem_item_id").joins("join subscribes on subscribes.gem_item_id = notifications.gem_item_id").where("notifications.created_at between :start_time and :end_time", {:start_time => Time.now.yesterday, :end_time => Time.now}).where("subscribes.user_id = ?", self.id)
   end
   
+  def subscribe_gems(gems)
+    gem_items = GemItem.where(:name => gems)
+    gem_items.each do |gem_item|
+      unless user.subscribed?(gem_item)
+        Subscribe.create({:gem_item => gem_item, :user => self})
+      end
+    end
+  end
+  
 end
